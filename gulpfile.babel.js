7cfcaec6ae77e698191db92529b659f6f81e1e93
dsgrant <danny@dsgrant.com>
2016-01-13 14:46:58 -0800

    add src folder, check imports are working correctly

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,9 @@
 'use strict';
 
+// Need gulp
 import gulp from 'gulp';
+
+// Need gulp
+import init from './src';
+
+init(gulp);
---
+++ src/index.js
@@ -0,0 +1,6 @@
+export default function(oGulp) {
+  const gulp = oGulp;
+  gulp.task('default', function() {
+    // place code for your default task here
+  });
+}
